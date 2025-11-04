761e1aa673c120ffd2ad9c06ab95084f64682b45
kikiririkakabubuoobabakeke <s2kt2s123456789@gmail.com>
2025-11-04 10:00:45 +0900

    変更

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -5,6 +5,8 @@ const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001/api/v1',
   headers: {
     'Content-Type': 'application/json',
+    'Accept': 'application/json',
+
   },
 });
 
